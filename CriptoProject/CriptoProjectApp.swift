c39324c02888290ca5ba9ddb6af4b1a2a12c7d08
Working always <kauamcm@gmail.com>
2024-08-02 21:03:14 -0300

    Initial Commit

---
+++ CriptoProject/CriptoProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CriptoProjectApp.swift
+//  CriptoProject
+//
+//  Created by Kaua Miguel on 02/08/24.
+//
+
+import SwiftUI
+
+@main
+struct CriptoProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
